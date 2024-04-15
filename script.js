46275bb07cf0942eea768ca4736f80867a35f5d5
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2024-04-15 14:08:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,66 +23,54 @@ const replaceChars = {
   "〔": "︹",
   "〕": "︺"
 };
-
 function genText() {
   const content = tategaki(document.getElementById('content').value, "text");
   document.getElementById('output').value = content;
-
   const tempInput = document.createElement('input');
   tempInput.value = content;
   document.body.appendChild(tempInput);
   tempInput.select();
   document.body.removeChild(tempInput);
 }
-
 function genHTML() {
   const content = tategaki(document.getElementById('content').value, "html");
   document.getElementById('output').value = content;
-
   const tempInput = document.createElement('input');
   tempInput.value = content;
   document.body.appendChild(tempInput);
   tempInput.select();
   document.body.removeChild(tempInput);
 }
-
 function genScript() {
   const content = tategaki(document.getElementById('content').value, "script");
   document.getElementById('output').value = content;
-
   const tempInput = document.createElement('input');
   tempInput.value = content;
   document.body.appendChild(tempInput);
   tempInput.select();
   document.body.removeChild(tempInput);
 }
-
 function tategaki(content, type) {
 	const convertedContent = halfWidthToFullWidth(content);
 	const convertedJapaneseContent = halfWidthToFullWidth(convertedContent)
 	const replacedContent = replaceMultiple(convertedJapaneseContent, replaceChars);
   const splitedContent = convertTo2DArray(replacedContent);
   let result;
-
   if (type === "text") result = reconstructForText(splitedContent);
   if (type === "html") result = reconstructForHTML(splitedContent);
   if (type === "script") result = reconstructForScript(splitedContent);
-
   return result;
 }
-
 function copyText() {
   var textArea = document.getElementById("output");
   textArea.select();
   document.execCommand("copy");
   alert("クリップボードにコピーしました！");
 }
-
 function convertTo2DArray(content) {
   const lines = content.split('\n');
   const maxLength = Math.max(...lines.map(line => line.length));
   const result = [];
-
   lines.forEach(line => {
     const characters = line.split('');
     while (characters.length < maxLength) {
@@ -90,10 +78,8 @@ function convertTo2DArray(content) {
     }
     result.push(characters);
   });
-
   return result;
 }
-
 function halfWidthToFullWidth(str) {
   let result = '';
   for (let i = 0; i < str.length; i++) {
@@ -105,8 +91,7 @@ function halfWidthToFullWidth(str) {
     }
   }
   return result;
-}
-
+};
 function kanaHalfToFull(str) {
   const kanaMap = {
     'ｶﾞ': 'ガ', 'ｷﾞ': 'ギ', 'ｸﾞ': 'グ', 'ｹﾞ': 'ゲ', 'ｺﾞ': 'ゴ',
@@ -129,13 +114,11 @@ function kanaHalfToFull(str) {
     'ｯ': 'ッ', 'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ',
     '｡': '。', '､': '、', 'ｰ': '｜', '･': '・'
 	};
-
   const reg = new RegExp('(' + Object.keys(kanaMap).join('|') + ')', 'g');
   return str.replace(reg, function (match) {
     return kanaMap[match];
   }).replace(/ﾞ/g, '゛').replace(/ﾟ/g, '゜');
 };
-
 function replaceMultiple(str, replacements) {
 	let result = str;
 	for (let pattern in replacements) {
@@ -145,10 +128,8 @@ function replaceMultiple(str, replacements) {
   }
   return result;
 }
-
 function reconstructForHTML(array) {
   let reconstructedText = '';
-
   for (let i = 0; i < array[0].length; i++) {
     for (let j = array.length - 1; j >= 0; j--) {
       if (array[j][i] !== undefined) {
@@ -156,14 +137,11 @@ function reconstructForHTML(array) {
       }
     }
     reconstructedText += '<br>\n';
-  }
-  
+  }  
   return reconstructedText;
 }
-
 function reconstructForScript(array) {
   let reconstructedText = '';
-
   for (let i = 0; i < array[0].length; i++) {
     for (let j = array.length - 1; j >= 0; j--) {
       if (array[j][i] !== undefined) {
@@ -171,14 +149,11 @@ function reconstructForScript(array) {
       }
     }
     reconstructedText += '\\n';
-  }
-  
+  }  
   return reconstructedText;
 }
-
 function reconstructForText(array) {
   let reconstructedText = '';
-
   for (let i = 0; i < array[0].length; i++) {
     for (let j = array.length - 1; j >= 0; j--) {
       if (array[j][i] !== undefined) {
@@ -186,11 +161,9 @@ function reconstructForText(array) {
       }
     }
     reconstructedText += '\n';
-  }
-  
+  }  
   return reconstructedText;
 }
-
 function checkWidth(char) {
   return char.match(/^[^\x01-\x7E\uFF61-\uFF9F]+$/);
 }
